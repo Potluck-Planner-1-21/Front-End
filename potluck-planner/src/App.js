0b83a85a3f3f06fac127f8649a9dde8268c2f474
ckirk2004 <ckirk2004@gmail.com>
2021-01-28 20:55:56 -0600

    Added login page, should be working with dummy data. still needs styling.

--- potluck-planner/src/App.js
+++ potluck-planner/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import LoginForm from './components/LoginForm';
 
 function App() {
+
+  //login testing data
+  const adminUser = {
+    email: 'admin@admin.com',
+    password: 'admin'
+  }
+
+  const [user, setUser] = useState ({ name: '', email:'' });
+  const [error, setError] = useState('');
+
+  const Login = details => {
+    console.log(details);
+
+    if (details.email == adminUser.email && details.password == adminUser.password){
+    console.log('Login Successful');
+    setUser({
+      name: details.name,
+      email: details.email
+    });
+  } else {
+    console.log('Email or Password is Incorrect');
+    setError('Email or Password is Incorrect');
+  }
+  }
+  const Logout = () => {
+    console.log('Logout');
+    setUser({ name: '', email:'' });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(user.email != '') ? (
+        <div className='welcome'>
+          <h2>Welcome <span>{user.name}</span></h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ) : (<LoginForm Login={Login} error={error} />)}
     </div>
   );
 }
