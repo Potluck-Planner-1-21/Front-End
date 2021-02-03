d577c31cf67ba50de4bf8eb2d24e959ca97d0d29
Rezaur Rahman <rezaur.rahman_nyc@icloud.com>
2021-02-03 12:59:04 -0500

    fixing

--- potluck-app/src/component/Login.js
+++ potluck-app/src/component/Login.js
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 const Login = (props) => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+  
     const initialState = {
          name: "",
          password: ""
