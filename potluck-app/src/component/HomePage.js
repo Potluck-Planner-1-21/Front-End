a1aeb6bc37c5a1f369e24859dce32320fae2de41
Rezaur Rahman <rezaur.rahman_nyc@icloud.com>
2021-02-04 19:15:25 -0500

    CreateEvent-UserParticipation

--- potluck-app/src/component/HomePage.js
+++ potluck-app/src/component/HomePage.js
@@ -3,6 +3,7 @@ import { Link, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CreateEvent from './CreateEvent';
 import OngoingEvents from './OngoingEvents';
 import EventGallery from './EventGallery';
+import Users from './Users';
 
 
 
@@ -11,16 +12,21 @@ function HomePage() {
     return (
     <div>
         <h1>Home Page</h1>
+        <h3>
             <Router>
                 <Link to='/create-event' >Create New Event</Link>
                 <Link to='/ongoing-events' >Ongoing Events</Link>
                 <Link to='/event-gallery' >Event Gallery</Link>
-                <Link to='/user' >User participation</Link>
+                <Link to='/users' >User participation</Link>
 
                 <Route exact path={'/create-event'} component={CreateEvent} />
                 <Route exact path={'/ongoing-events'} component={OngoingEvents} />
                 <Route exact path={'/event-gallery'} component={EventGallery} />
+                <Switch>
+                <Route exact path={'/users'} component={Users} />
+                </Switch>
             </Router>
+            </h3>
 
             
     </div>
@@ -28,33 +34,3 @@ function HomePage() {
 }
 
 export default HomePage;
-
-
-
-
-
-
-/*
-function HomePage() {
-
-    return (
-    <div>
-        <h1>Home Page</h1>
-            <Router>
-                <Link to='/create-event' >Create New Event</Link>
-                <Link to='/ongoing-events' >Ongoing Events</Link>
-                <Link to='/event-gallery' >Event Gallery</Link>
-                <Link to='/user' >User participation</Link>
-
-                <Route exact path={'/create-event'} component={CreateEvent} />
-                <Route exact path={'/ongoing-events'} component={OngoingEvents} />
-                <Route exact path={'/event-gallery'} component={EventGallery} />
-            </Router>
-
-            
-    </div>
-    )
-}
-
-export default HomePage
-*/
